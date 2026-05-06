3e1a33117ff2daa674750becde1aab08c480e513
pkmer <q10viking@outlook.com>
2026-05-06 09:50:56 +0800

    chore(project): 初始化项目配置并更新日志格式
    - 更新日志格式以包含文件名和行号信息
    - 将加载的图片资源从 logo.png 更改为 Player.png
    - 添加 .python-version 文件指定 Python 版本为 3.13
    - 创建 pyproject.toml 文件配置项目依赖和元数据
    - 添加 pygame 作为项目依赖项

--- src/game/crazydriver/v3/main.py
+++ src/game/crazydriver/v3/main.py
@@ -6,8 +6,7 @@ if __name__ == '__main__':
     # 配置日志
     logging.basicConfig(
         level=logging.DEBUG,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d:  %(message)s',
         datefmt='%H:%M:%S'
-
     )
-    ResourceManager().load_image('logo.png')
+    ResourceManager().load_image('Player.png')
